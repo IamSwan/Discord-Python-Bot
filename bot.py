cf15b89456c8dc1c24d2a0a880ef8f8ccc0dd313
Swan Sarli <swan.sarli@epitech.eu>
2023-02-01 21:22:51 +0100

    Reworked the structure of the code. Added a few commands.

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import discord
+from discord.ext import commands
 import sys
 import os
 import dotenv
@@ -6,39 +7,67 @@ import dotenv
 config = dotenv.load_dotenv(".env")
 token = os.getenv("TOKEN")
 
-client = discord.Client(intents=discord.Intents.all())
+client = commands.Bot(intents=discord.Intents.all(), command_prefix="$")
 
-prefix = "$"
-
-latency = 0
+client.remove_command("help")
 
 @client.event
 async def on_ready():
     print("Bot is ready.")
-    
+        
 @client.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.MissingPermissions):
+        await ctx.send("You do not have the permissions to use this command.")
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send("You are missing a required argument.")
+    else:
+        await ctx.send(f"An error occured: {error}")
+        
+@client.command()
+@commands.has_permissions(ban_members=True)
+async def avadakedavra(ctx, member: discord.Member, *, reason=None):
+    await member.ban(reason = reason)
+    await ctx.send(f"Banned {member.mention}")
+
+@client.command()
+async def help(ctx):
+    if ctx.channel.name == "bot-cmds" or ctx.author.guild_permissions.manage_messages:
+        await ctx.send(f"**__Help__** \n    **$trello** - Get the link to the Trello board\n    **$faq** - Answers to the most common questions \n    **$links** - Links to the official group, the game's page\n    **$ping** - Pong!\n    **$help** - Shows this message \n*There's also a few easter eggs only for admins*")
+
+@client.command()
+async def trello(ctx):
+    if ctx.channel.name == "bot-cmds" or ctx.author.guild_permissions.manage_messages:
+        await ctx.send("**trello link:** (none for now)")
+
+@client.command()
+async def faq(ctx):
+    if ctx.channel.name == "bot-cmds" or ctx.author.guild_permissions.manage_messages:
+        await ctx.send("**FAQ:** (none for now)")
+
+@client.command()
+async def links(ctx):
+    if ctx.channel.name == "bot-cmds" or ctx.author.guild_permissions.manage_messages:
+        await ctx.send("**links:** (none for now)")
+
+@client.command()
+async def ping(ctx):
+    latency = client.latency
+    latency = str(latency)
+    latency = latency[0:5]
+    await ctx.send(f"pong! :troll: \nlatency: {str(latency)} ms")
+
+
+@client.listen('on_message')
 async def on_message(msg: discord.Message):
     if msg.author == client.user:
         return
     if msg.content.__contains__("rockyuwu"):
         await msg.channel.send("rocky owner UwU.")
 
-    if msg.content == f"{str(prefix)}ping":
-        latency = client.latency
-        await msg.channel.send(f"pong! :troll: \nlatency: {str(latency)} ms")
-
-    if msg.content.startswith(str(prefix)):
-        if msg.channel.name == "bot-cmds" or msg.author.guild_permissions.manage_messages:
-            if msg.content == f"{str(prefix)}help":
-                await msg.channel.send(f"**__Help__** \n    **{prefix}trello** - Get the link to the Trello board\n    **{prefix}faq** - Answers to the most common questions \n    **{prefix}links** - Links to the official group, the game's page\n")
-            if msg.content == f"{str(prefix)}trello":
-                await msg.channel.send("**trello link:** (none for now)")
-            if msg.content == f"{str(prefix)}faq":
-                await msg.channel.send("**FAQ:** (none for now)")
-            if msg.content == f"{str(prefix)}links":
-                await msg.channel.send("**links:** (none for now)")
     if msg.author.id == 291463810449932298 and msg.content.lower() == "bot, stop":
-        await msg.channel.send("ok :(")
+        await msg.channel.send("*ok :(*\n**shutting down...**")
         sys.exit()
+
 
 client.run(token)
